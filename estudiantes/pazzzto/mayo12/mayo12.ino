0bfa3100882cbdf0b84e6f0f034c0ef592b7d92d
Pazzzto <128753779+Pazzzto@users.noreply.github.com>
2023-05-12 18:08:24 -0400

    Add files via upload

---
+++ estudiantes/pazzzto/mayo12/mayo12.ino
@@ -0,0 +1,17 @@
+// Wiring / Arduino Code
+// Code for sensing a switch status and writing the value to the serial port.
+
+int pinPerilla = A0;                       // Switch connected to pin 4
+int valorPerilla = -1;
+
+void setup() {
+  Serial.begin(9600);                    // Start serial communication at 9600 bps
+}
+
+void loop() {
+
+  valorPerilla = analogRead(pinPerilla);
+  Serial.write(valorPerilla);
+
+  delay(100);                            // Wait 100 milliseconds
+}
