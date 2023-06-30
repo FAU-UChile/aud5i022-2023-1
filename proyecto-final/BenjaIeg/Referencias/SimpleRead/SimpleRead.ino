86948f500cbbaa2a3a512bb2b289b0dd7d71064b
Benjaieg <128185999+BenjaIeg@users.noreply.github.com>
2023-06-30 08:58:30 -0400

    Add files via upload

---
+++ proyecto-final/BenjaIeg/Referencias/SimpleRead/SimpleRead.ino
@@ -0,0 +1,15 @@
+int switchPin = 4;                       // Switch connected to pin 4
+
+void setup() {
+  pinMode(switchPin, INPUT);             // Set pin 0 as an input
+  Serial.begin(9600);                    // Start serial communication at 9600 bps
+}
+
+void loop() {
+  if (digitalRead(switchPin) == HIGH) {  // If switch is ON,
+    Serial.write(1);               // send 1 to Processing
+  } else {                               // If the switch is not ON,
+    Serial.write(0);               // send 0 to Processing
+  }
+  delay(1000);                            // Wait 100 milliseconds
+}
